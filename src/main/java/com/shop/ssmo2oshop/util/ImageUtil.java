76afec5db57bd2d06faa4215e475f5d369f2b225
brandon0824 <446038117@qq.com>
2020-07-25 14:54:25 +0800

    thumbnail config

---
+++ src/main/java/com/shop/ssmo2oshop/util/ImageUtil.java
@@ -0,0 +1,12 @@
+package com.shop.ssmo2oshop.util;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+public class ImageUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thumbnails.of(new File(""))
+	}
+
+}
